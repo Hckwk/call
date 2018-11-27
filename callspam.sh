73987282ade3e7211956e3f77bdfef2b71551f82
Hckwk <45328175+Hckwk@users.noreply.github.com>
2018-11-27 11:38:21 +0700

    Add files via upload

---
+++ callspam.sh
@@ -0,0 +1,56 @@
+call.sh
+
+#!/bin/sh
+
+#This spam from Longan
+
+
+echo Selamat datang bro, Siapa nick lo? #tulisan keluar
+
+
+read nick #membaca yang ditulis
+
+echo "================================================="
+
+echo "== ╦ ╦┌─┐ ┬┌┐┌┌┬┐┐┌┌─┐┌─┐┬┌─┐ =="
+
+echo "== ╠═
+ "================================================="
+
+echo selamat datang $nick ":)"
+
+echo
+
+echo "Silahkan masukan nomor telp target"
+
+echo contoh 0812345678
+
+read target # masukin no telp
+
+
+echo
+
+echo Apakah nomor $target "sudah benar?"
+
+echo y/n?
+
+read confirm
+
+echo
+
+if [ $confirm = "y" ]; then
+
+echo Melakukan spam call ke nomor $target
+
+target_do='http://zpro.000webhostapp.com/api/call.php?nomor='$target
+
+CURL_RESPONSE=`curl -s -o /dev/null -w "%{http_code}" $target_do`
+
+#echo $target_do
+
+else
+
+echo Salah, silahkan coba lagi
+
+fi
+
